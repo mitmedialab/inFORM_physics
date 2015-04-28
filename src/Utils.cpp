c48781565d68bd22911e406afd98bc27738ede1b
Daniel Windham <dewindham@gmail.com>
2015-04-28 11:49:06 -0400

    utils: add utils file with clockInSeconds function

---
+++ src/Utils.cpp
@@ -0,0 +1,14 @@
+//
+//  Utils.cpp
+//  Relief2
+//
+//  Created by Daniel Windham on 4/28/15.
+//
+//
+
+#include "Utils.h"
+
+
+double clockInSeconds() {
+    return clock() / (double) CLOCKS_PER_SEC;
+}
---
+++ src/Utils.h
@@ -0,0 +1,19 @@
+//
+//  Utils.h
+//  Relief2
+//
+//  Created by Daniel Windham on 4/28/15.
+//
+//
+
+
+#ifndef __Relief2__Utils__
+#define __Relief2__Utils__
+
+
+#include <ctime>
+
+
+double clockInSeconds();
+
+#endif /* defined(__Relief2__Utils__) */
