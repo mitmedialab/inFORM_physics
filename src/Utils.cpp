6545b344bcfa59c67801f02442fcc54915dc670b
Daniel Windham <dewindham@gmail.com>
2015-04-30 12:33:51 -0400

    utils: add Rectangle class

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -9,6 +9,16 @@
 #include "Utils.h"
 
 
+
+Rectangle::Rectangle(float left, float top, float width, float height) :
+        left(left), top(top), width(width), height(height) {
+}
+
+bool Rectangle::containsPoint(ofPoint &point) {
+    return left < point.x && point.x < left + width && top < point.y && point.y < top + height;
+}
+
+
 double clockInSeconds() {
     return clock() / (double) CLOCKS_PER_SEC;
 }
--- src/Utils.h
+++ src/Utils.h
@@ -12,8 +12,22 @@
 
 
 #include <ctime>
+#include "ofMain.h"
+
+
+class Rectangle {
+public:
+    Rectangle(float left=0, float top=0, float width=0, float height=0);
+    bool containsPoint(ofPoint &point);
+
+    float left;
+    float top;
+    float width;
+    float height;
+};
 
 
 double clockInSeconds();
+bool pointIsInsideRect(ofPoint &point, Rectangle &rect);
 
 #endif /* defined(__Relief2__Utils__) */
