1b7234c02128ce0e60658e6c3b1e35627ec78c9c
Daniel Windham <dewindham@gmail.com>
2015-05-04 11:42:50 -0400

    math: add polynomial equation solvers

--- src/MathUtils.h
+++ src/MathUtils.h
@@ -14,7 +14,17 @@
 #define __Relief2__MathUtils__
 
 
+#define MATH_EPSILON 1e-9
+
+
 double factorial(int n, int k=1);
 double binomialCoefficient(int n, int k);
+
+// polynomial root finders (polynomials up to degree 4) by Jochen Scharze
+// originally found at http://tog.acm.org/resources/GraphicsGems/gemsiv/vec_mat/ray/solver.h
+int isZero(double x);
+int solveQuadratic(double c[3], double s[2]);
+int solveCubic(double c[4], double s[3]);
+int solveQuartic(double c[5], double s[4]);
 
 #endif /* defined(__Relief2__MathUtils__) */
